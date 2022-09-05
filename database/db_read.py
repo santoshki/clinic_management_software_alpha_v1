6ab9d38db5b691e6350bcd555fa13635e19af46a
santoshki <santoshkdevp@gmail.com>
2022-09-06 00:11:52 +0530

    db file added for solution data insertion.

--- database/db_read.py
+++ database/db_read.py
@@ -1,9 +1,31 @@
 import sqlite3
 
-conn = sqlite3.connect('patient_entry_data.db')
 
-cursor = conn.cursor()
-cursor.execute("""SELECT * from patient_entry_data""")
+def fetch_all_data():
+    try:
+        conn = sqlite3.connect('patient_entry_data.db')
+        cursor = conn.cursor()
+        cursor.execute("""SELECT * from patient_entry_data""")
+        result = cursor.fetchall();
+        print("Data stored in db:", result)
 
-result = cursor.fetchall();
-print("Data stored in db:", result)
+    except Exception as e:
+        print("Exception occurred:", e)
+
+
+def fetch_data_health_issue(query):
+    try:
+
+        conn = sqlite3.connect('patient_entry_data.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM patient_entry_data WHERE HEALTHISSUE=?", (query,))
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+
+    except Exception as e:
+        print("Exception occurred:", e)
+
+
+if __name__ == '__main__':
+    fetch_data_health_issue("headache")
--- entities/entity.py
+++ entities/entity.py
@@ -10,12 +10,17 @@ def login():
     return render_template('login_form.html')
 
 
-@app.route('/patient_entry_form', methods=['GET', 'POST'])
+@app.route('/patient_entry_form/', methods=['GET', 'POST'])
 def patient_entry_form():
     return render_template('patient_entry_form.html')
 
 
-@app.route('/patient_data_recorded', methods=['GET', 'POST'])
+@app.route('/patient_vitals_data/', methods=['GET', 'POST'])
+def patient_vitals_data():
+    return render_template('patient_vitals_form.html')
+
+
+@app.route('/patient_data_recorded/', methods=['GET', 'POST'])
 def patient_data_recorded():
     if request.method == "POST":
         patient_entry_form_data = []
@@ -23,7 +28,7 @@ def patient_data_recorded():
         try:
             form_data = request.form
             patient_entry_form_data = entity_parser.entry_data_parser(form_data)
-            db_entity.db_insert(patient_entry_form_data)
+            db_entity.db_insert_entry_data(patient_entry_form_data)
             return "Patient data recorded successfully."
         except Exception as e:
             print("Exception occurred:", e)
